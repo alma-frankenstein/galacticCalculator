fcdb7a3c8f67f34012bb8f22ad1126d2b4bc8fde
Alma <a.q.frankenstein@gmail.com>
2020-09-04 10:56:52 -0700

    pass test for showing your age on a given planet as a string

--- __tests__/earth.test.js
+++ __tests__/earth.test.js
@@ -3,8 +3,13 @@ import Calculator from '../src/js/planets';
 describe('Calculator', () => {
 
   test('should take in an age and return it unchanged', () => {
-
-    const earthCalc = new Calculator(60, 'Earth');
-    expect(earthCalc.earthAge).toEqual(60);
+    const calc = new Calculator(60, 'Earth');
+    expect(calc.earthAge).toEqual(60);
   });
+
+  test('should treturn template literal with earth age and planet age', () => {
+    const calc = new Calculator(60, 'Earth');
+    expect(calc.ageCalculator()).toEqual("Your age on Earth is 60");
+  });
+  
 });
--- src/js/planets.js
+++ src/js/planets.js
@@ -1,21 +1,26 @@
 export default class Calulator {
   constructor(earthAge, planet) {
     this.earthAge = earthAge;
+    this.planet = planet;
     this.planetAge = 0;
   }
 
   ageCalculator() {
     if(this.planet === 'Earth') {
       this.planetAge = this.calcEarthAge(this.earthAge);
+    } else if (this.planet === 'Mercury') {
+      this.planetAge = calcMercuryAge(this.earthAge);
     }
-    return `Your age on ${this.planetAge} is ${this.earthAge}`
+    return `Your age on ${this.planet} is ${this.planetAge}`
   }
 
   calcEarthAge() {
     return this.earthAge * 1;
   }
 
-  // mercuryAge()
+  calcMercuryAge() {
+    return Math.round(this.earthAge/.24);
+  }
 }
 
 
