5045e154ecaf46fe8dc4492b124a4bba58ea55cb
Alma <a.q.frankenstein@gmail.com>
2020-09-04 12:18:53 -0700

    add Jupiter function, get passing test for it

---
+++ __tests__/jupiter.test.js
@@ -0,0 +1,14 @@
+import Calculator from '../src/js/planets';
+
+describe('Calculator', () => {
+
+  test('should take in an age, divide it by 11.86, and return as an integer', () => {
+    const calc = new Calculator(60, 'Jupiter');
+    expect(calc.calcJupiterAge()).toEqual(5);
+  });
+
+  test('should treturn template literal telling the user their age on Jupiter', () => {
+    const calc = new Calculator(60, 'Jupiter');
+    expect(calc.ageCalculator()).toEqual("Your age on Jupiter is 5");
+  });
+});
